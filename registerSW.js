126e883303b71d573d941de8aca2119765c16aa3
Alich <avida.chatraee2001@yahoo.com>
2025-06-27 14:08:49 +0330

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/git-commands-guide/sw.js', { scope: '/git-commands-guide/' })})}
